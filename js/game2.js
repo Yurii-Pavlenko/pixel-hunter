6432f61e525e1a8085e734219b9b458134ab402a
Yurii Pavlenko <mark.orefa@gmail.com>
2017-10-03 07:59:21 +0300

    #4 Тесные связи. Поправил представление одиночных тегов

--- js/game2.js
+++ js/game2.js
@@ -7,28 +7,28 @@ const game2 = getElementFromTemplate(`
   <header class="header">
     <div class="header__back">
       <button class="back">
-        <\img src="img/arrow_left.svg" width="45" height="45" alt="Back">
-        <\img src="img/logo_small.svg" width="101" height="44">
+        <img src="img/arrow_left.svg" width="45" height="45" alt="Back">
+        <img src="img/logo_small.svg" width="101" height="44">
       </button>
     </div>
     <h1 class="game__timer">NN</h1>
     <div class="game__lives">
-      <\img src="img/heart__empty.svg" class="game__heart" alt="Life" width="32" height="32">
-      <\img src="img/heart__full.svg" class="game__heart" alt="Life" width="32" height="32">
-      <\img src="img/heart__full.svg" class="game__heart" alt="Life" width="32" height="32">
+      <img src="img/heart__empty.svg" class="game__heart" alt="Life" width="32" height="32">
+      <img src="img/heart__full.svg" class="game__heart" alt="Life" width="32" height="32">
+      <img src="img/heart__full.svg" class="game__heart" alt="Life" width="32" height="32">
     </div>
   </header>
   <div class="game">
     <p class="game__task">Угадай, фото или рисунок?</p>
     <form class="game__content  game__content--wide">
       <div class="game__option">
-        <\img src="http://placehold.it/705x455" alt="Option 1" width="705" height="455">
+        <img src="http://placehold.it/705x455" alt="Option 1" width="705" height="455">
         <label class="game__answer  game__answer--photo">
-          <\input name="question1" type="radio" value="photo">
+          <input name="question1" type="radio" value="photo">
           <span>Фото</span>
         </label>
         <label class="game__answer  game__answer--wide  game__answer--paint">
-          <\input name="question1" type="radio" value="paint">
+          <input name="question1" type="radio" value="paint">
           <span>Рисунок</span>
         </label>
       </div>
--- js/game3.js
+++ js/game3.js
@@ -7,28 +7,28 @@ const game3 = getElementFromTemplate(`
   <header class="header">
     <div class="header__back">
       <button class="back">
-        <\img src="img/arrow_left.svg" width="45" height="45" alt="Back">
-        <\img src="img/logo_small.svg" width="101" height="44">
+        <img src="img/arrow_left.svg" width="45" height="45" alt="Back">
+        <img src="img/logo_small.svg" width="101" height="44">
       </button>
     </div>
     <h1 class="game__timer">NN</h1>
     <div class="game__lives">
-      <\img src="img/heart__empty.svg" class="game__heart" alt="Life" width="32" height="32">
-      <\img src="img/heart__full.svg" class="game__heart" alt="Life" width="32" height="32">
-      <\img src="img/heart__full.svg" class="game__heart" alt="Life" width="32" height="32">
+      <img src="img/heart__empty.svg" class="game__heart" alt="Life" width="32" height="32">
+      <img src="img/heart__full.svg" class="game__heart" alt="Life" width="32" height="32">
+      <img src="img/heart__full.svg" class="game__heart" alt="Life" width="32" height="32">
     </div>
   </header>
   <div class="game">
     <p class="game__task">Найдите рисунок среди изображений</p>
     <form class="game__content  game__content--triple">
       <div class="game__option">
-        <\img src="http://placehold.it/304x455" alt="Option 1" width="304" height="455">
+        <img src="http://placehold.it/304x455" alt="Option 1" width="304" height="455">
       </div>
       <div class="game__option  game__option--selected">
-        <\img src="http://placehold.it/304x455" alt="Option 1" width="304" height="455">
+        <img src="http://placehold.it/304x455" alt="Option 1" width="304" height="455">
       </div>
       <div class="game__option">
-        <\img src="http://placehold.it/304x455" alt="Option 1" width="304" height="455">
+        <img src="http://placehold.it/304x455" alt="Option 1" width="304" height="455">
       </div>
     </form>
     <div class="stats">
