0377304dc17d54577d04068953bc7fe6f17fd82a
Yurii Pavlenko <mark.orefa@gmail.com>
2017-10-04 20:49:45 +0300

    #5 Убрал комментарии из gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -13,7 +13,7 @@ const mqpacker = require('css-mqpacker');
 const minify = require('gulp-csso');
 const rename = require('gulp-rename');
 const imagemin = require('gulp-imagemin');
-const mocha = require('gulp-mocha'); // Добавим установленный gulp-mocha плагин
+const mocha = require('gulp-mocha');
 
 gulp.task('style', function () {
   return gulp.src('sass/style.scss')
@@ -51,8 +51,8 @@ gulp.task('test', function () {
   return gulp
     .src(['js/**/*.test.js'], { read: false })
     .pipe(mocha({
-      compilers: ['js:babel-register'], // Включим поддержку "import/export" в Mocha тестах
-      reporter: 'spec'       // Вид в котором я хочу отображать результаты тестирования
+      compilers: ['js:babel-register'],
+      reporter: 'spec'
     }));
 });
 
