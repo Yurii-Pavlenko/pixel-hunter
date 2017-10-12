0744f96bc1f79ce9ebe9fb1cb4f903277f341af9
Yurii Pavlenko <mark.orefa@gmail.com>
2017-10-12 14:35:19 +0300

    #6 Начнем с конца. Сделал стрелочные функции и более "читабельным" код

--- js/utils/count-score.js
+++ js/utils/count-score.js
@@ -3,26 +3,35 @@ const countScore = (answers, amountOfLives) => {
   const correctAnswerScore = 100;
   const quickAnswerScore = 150;
   const slowAnswerScore = 50;
-  const scoreForLives = 50;
+  const livesScore = 50;
   const minTruthyAnswersAmount = 7;
 
   if (answers.length < arrayLength && !amountOfLives) {
     return -1;
   }
 
-  const trueAnswers = answers.filter(function (element) {
+  const trueAnswers = answers.filter((element) => {
     return element === correctAnswerScore;
   });
 
-  const fastAnswers = answers.filter(function (element) {
+  const fastAnswers = answers.filter((element) => {
     return element === quickAnswerScore;
   });
 
-  const slowAnswers = answers.filter(function (element) {
+  const slowAnswers = answers.filter((element) => {
     return element === slowAnswerScore;
   });
 
-  return trueAnswers.length + fastAnswers.length + slowAnswers.length >= minTruthyAnswersAmount ? trueAnswers.length * correctAnswerScore + fastAnswers.length * quickAnswerScore + slowAnswers.length * slowAnswerScore + amountOfLives * scoreForLives : -1;
+  const scoreForRightAnswers = trueAnswers.length * correctAnswerScore;
+  const scoreForFastAnswers = fastAnswers.length * quickAnswerScore;
+  const scoreForSlowAnswers = slowAnswers.length * slowAnswerScore;
+  const scoreForLives = amountOfLives * livesScore;
+
+  const scoreForAllAnswers = scoreForRightAnswers + scoreForFastAnswers + scoreForSlowAnswers + scoreForLives;
+
+  const truthyAnswersAmount = trueAnswers.length + fastAnswers.length + slowAnswers.length;
+
+  return truthyAnswersAmount >= minTruthyAnswersAmount ? scoreForAllAnswers : -1;
 
 };
 
