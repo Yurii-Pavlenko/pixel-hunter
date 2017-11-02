3a19e5a268918605766dbd9c8e1a56d493a96d84
Yurii Pavlenko <mark.orefa@gmail.com>
2017-11-02 10:26:24 +0200

    #8 Модельный показ. Сделал view + модуль для третьего игрового экрана game3-view & game3

--- js/screens/game3/game3-data.js
+++ js/screens/game3/game3-data.js
@@ -6,50 +6,7 @@ import renderScreen from "../../utils/screen-renderer";
 const endOfGame = 10;
 
 export const game3Data = {
-  description: `Найдите рисунок среди изображений`,
-  /* jumpTo: {
-    next: game1
-  }*/
-/*
-  pictures: [
-    {
-      imgSrc: `http://fotografy.ru/wp-content/uploads/2015/01/1711.jpg`,
-      imgType: `photo`
-    },
-
-    {
-      imgSrc: `https://artrue.ru/wp-content/uploads/2017/02/yigal-ozeri-06.jpg`,
-      imgType: `paint`
-    },
-    {
-      imgSrc: `http://fotografy.ru/wp-content/uploads/2015/01/1811.jpg`,
-      imgType: `photo`
-    },
-    {
-      imgSrc: `https://artrue.ru/wp-content/uploads/2017/02/yigal-ozeri-05.jpg`,
-      imgType: `paint`
-    },
-    {
-      imgSrc: `https://www.nrdc.org/sites/default/files/styles/full_content--retina/public/media-uploads/coms11_path_house_2400.jpg?itok=3nK5tmPF`,
-      imgType: `photo`
-    },
-    {
-      imgSrc: `http://i.imgur.com/DKR1HtB.jpg`,
-      imgType: `photo`
-    },
-    {
-      imgSrc: `https://artrue.ru/wp-content/uploads/2017/02/yigal-ozeri-03.jpg`,
-      imgType: `paint`
-    },
-    {
-      imgSrc: `http://fotografy.ru/wp-content/uploads/2015/01/1711.jpg`,
-      imgType: `photo`
-    },
-    {
-      imgSrc: `https://artrue.ru/wp-content/uploads/2017/02/yigal-ozeri-06.jpg`,
-      imgType: `paint`
-    }
-  ]*/
+  IMG_NUMBER: 3
 };
 
 export const renderAnswers3 = (questions, answers3, answerType) => {
--- js/screens/game3/game3.js
+++ js/screens/game3/game3.js
@@ -1,53 +1,18 @@
-import getElementFromTemplate from "../../utils/get-element-from-html";
-import gameHeader from "../game-header-view";
-import gameStats from "../game-stats";
-import {game3Data, renderAnswers3, checkNext3} from "./game3-data";
-import {chooseAnswerType, goBack, stateData} from "../play-data";
 import {getPictures} from "../../utils/get-pictures";
 import pictures from "../../utils/pictures";
+import {checkNext3, game3Data} from "./game3-data";
+import Game3View from "./game3-view";
+import {goBack, stateData} from "../play-data";
+import gameStats from "../game-stats";
 
-const IMG_NUMBER = 3;
 export default () => {
-  const imgArray = getPictures(pictures, IMG_NUMBER);
-  const game3 = getElementFromTemplate(`
-  ${gameHeader(stateData)}
-  <div class="game">
-    <p class="game__task">${game3Data.description}</p>
-    <form class="game__content  game__content--triple">
-      <div class="game__option">
-        <img src=${imgArray[0].imgSrc} alt="Option 1" width="304" height="455">
-      </div>
-      <div class="game__option">
-        <img src=${imgArray[1].imgSrc} alt="Option 1" width="304" height="455">
-      </div>
-      <div class="game__option">
-        <img src=${imgArray[2].imgSrc} alt="Option 1" width="304" height="455">
-      </div>
-    </form>
-    ${gameStats(stateData)}
-  </div>
-`);
-  const headerBack = game3.querySelector(`.header__back`);
-  const gameContent = Array.from(game3.querySelectorAll(`.game__option`));
-  const questions = [];
+  const imgArray = getPictures(pictures, game3Data);
 
-  gameContent.forEach((option) => {
-    option.onclick = () => {
-      option.classList.add(`game__option--selected`);
-      const answers3 = gameContent.map((opt) => {
-        return (opt.classList.contains(`game__option--selected`)) ? `paint` : `photo`;
-      });
-      for (let i = 0; i < answers3.length; i++) {
-        questions.push(imgArray[i].imgType);
-      }
-      renderAnswers3(questions, answers3, chooseAnswerType());
-      checkNext3(stateData);
-    };
-  });
+  const game3 = new Game3View(stateData, gameStats, imgArray[0], imgArray[1], imgArray[2]);
 
-  headerBack.onclick = () => {
-    goBack(stateData);
-  };
+  game3.onButtonClick = () => checkNext3(stateData);
+
+  game3.onBackClick = () => goBack(stateData);
 
   return game3;
 };
